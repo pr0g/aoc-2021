9d20dd3ed9b759d925fb1172974132f68cfb2a17
Tom Hulton-Harrop <tom.hultonharrop@gmail.com>
2021-12-03 23:12:13 +0000

    day 3, part 2 wip

--- day3/main.cpp
+++ day3/main.cpp
@@ -6,6 +6,15 @@
 #include <sstream>
 #include <vector>
 
+std::vector<int> number_from_string(const std::string& str)
+{
+  std::vector<int> number;
+  for (const char c : str) {
+    number.push_back(std::stoi(std::string(1, c)));
+  }
+  return number;
+}
+
 int decimal_from_binary(const std::vector<int>& binary)
 {
   int base = 1;
@@ -29,6 +38,63 @@ std::vector<int> flip(std::vector<int> binary)
   return binary;
 }
 
+std::vector<std::vector<int>> get_secondary_binary_numbers(
+  std::vector<std::string> binary_numbers)
+{
+  std::vector<std::vector<int>> secondary_binary_numbers;
+  secondary_binary_numbers.resize(binary_numbers.front().size());
+  for (const auto& binary_number : binary_numbers) {
+    int i = 0;
+    for (const auto digit : binary_number) {
+      secondary_binary_numbers[i++].push_back(std::stoi(std::string(1, digit)));
+    }
+  }
+  return secondary_binary_numbers;
+}
+
+std::vector<int> get_final_binary_number(
+  std::vector<std::vector<int>> secondary_binary_numbers, int bit)
+{
+  std::vector<int> final_binary_number;
+  for (const auto& secondary_binary_number : secondary_binary_numbers) {
+    auto interested_bits_set = std::count_if(
+      secondary_binary_number.begin(), secondary_binary_number.end(),
+      [bit](const int number) { return number == bit; });
+    auto uninterested_bits_set = secondary_binary_number.size() - interested_bits_set;
+    // need to switch to be min not max here... comparator
+    if (interested_bits_set >= uninterested_bits_set) {
+      final_binary_number.push_back(bit);
+    } else {
+      int other = bit == 1 ? 0 : 1;
+      final_binary_number.push_back(other);
+    }
+  }
+  return final_binary_number;
+}
+
+std::string find_number(
+  std::vector<int> final_binary_number, std::vector<std::string> binary_numbers, int bit)
+{
+  std::vector<std::string> next_binary_numbers;
+  for (int i = 0; i < final_binary_number.size(); ++i) {
+    for (auto binary_number : binary_numbers) {
+      auto digit = std::stoi(std::string(1, binary_number[i]));
+      if (digit == final_binary_number[i]) {
+        next_binary_numbers.push_back(binary_number);
+      }
+    }
+    binary_numbers = next_binary_numbers;
+    next_binary_numbers.clear();
+    if (binary_numbers.size() == 1) {
+      break;
+    }
+    final_binary_number =
+      get_final_binary_number(get_secondary_binary_numbers(binary_numbers), bit);
+  }
+
+  return binary_numbers.front();
+}
+
 int main(int argc, char** argv)
 {
   std::ifstream reader("input.txt");
@@ -37,33 +103,23 @@ int main(int argc, char** argv)
     binary_numbers.push_back(line);
   }
 
-  std::vector<std::vector<int>> secondary_binary_numbers;
-  secondary_binary_numbers.resize(binary_numbers.front().size());
+  auto secondary_binary_numbers = get_secondary_binary_numbers(binary_numbers);
+  auto final_binary_number_ones = get_final_binary_number(secondary_binary_numbers, 1);
 
-  for (const auto& binary_number : binary_numbers) {
-    int i = 0;
-    for (const auto digit : binary_number) {
-      secondary_binary_numbers[i++].push_back(std::stoi(std::string(1, digit)));
-    }
-  }
-
-  std::vector<int> final_binary_number;
-  for (const auto& secondary_binary_number : secondary_binary_numbers) {
-    auto one_bits_set = std::count_if(
-      secondary_binary_number.begin(), secondary_binary_number.end(),
-      [](const int number) { return number == 1; });
-
-    if (one_bits_set > secondary_binary_number.size() / 2) {
-      final_binary_number.push_back(1);
-    } else {
-      final_binary_number.push_back(0);
-    }
-  }
-
-  // std::cout << decimal_from_binary(final_binary_number) << '\n';
-
-  int gamma_rate = decimal_from_binary(final_binary_number);
-  int epsilon_rate = decimal_from_binary(flip(final_binary_number));
+  int gamma_rate = decimal_from_binary(final_binary_number_ones);
+  int epsilon_rate = decimal_from_binary(flip(final_binary_number_ones));
 
   std::cout << "part 1: " << gamma_rate * epsilon_rate << '\n';
+
+  // oxygen generator rating
+  // co2 scrubber rating
+
+  auto oxygen_str = find_number(final_binary_number_ones, binary_numbers, 1);
+  auto oxygen = decimal_from_binary(number_from_string(oxygen_str));
+  auto final_binary_number_zeros = get_final_binary_number(secondary_binary_numbers, 0);
+  auto co2_str = find_number(final_binary_number_zeros, binary_numbers, 0);
+  auto co2 = decimal_from_binary(number_from_string(co2_str));
+
+  int i;
+  i = 0;
 }
