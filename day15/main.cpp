4b6834368f1b853cf4c5f897d4befeeecfb9e860
Tom Hulton-Harrop <tom.hultonharrop@gmail.com>
2021-12-17 22:53:04 +0000

    day 15, part 2 complete

--- day15/main.cpp
+++ day15/main.cpp
@@ -15,27 +15,20 @@ struct offset_t
   int c;
 };
 
-struct path_node_t
-{
-  offset_t offset;
-  offset_t parent;
-  int cost;
-};
-
 struct node_t
 {
   offset_t offset;
   int cost = INT_MAX;
 };
 
-void bfs(const std::vector<std::vector<int>>& grid, const offset_t& goal)
+int bfs(const std::vector<std::vector<int>>& grid, const offset_t& goal)
 {
   auto find_neighbors = [&grid](
                           const offset_t& position,
-                          const std::vector<offset_t>& visited,
+                          const std::vector<node_t>& visited,
                           std::vector<node_t>& unvisited) {
     const offset_t offsets[] = {{0, -1}, {1, 0}, {0, 1}, {-1, 0}};
-    std::vector<offset_t> path;
+    std::vector<node_t> neighbors;
     for (const auto& offset : offsets) {
       if (
         position.c + offset.c >= 0 && position.c + offset.c < grid.back().size()
@@ -46,8 +39,8 @@ void bfs(const std::vector<std::vector<int>>& grid, const offset_t& goal)
           std::find_if(
             visited.begin(), visited.end(),
             [next_position](const auto& visited_node) {
-              return next_position.r == visited_node.r
-                  && next_position.c == visited_node.c;
+              return next_position.r == visited_node.offset.r
+                  && next_position.c == visited_node.offset.c;
             })
           == visited.end()) {
           auto visited_node = std::find_if(
@@ -67,11 +60,11 @@ void bfs(const std::vector<std::vector<int>>& grid, const offset_t& goal)
           if (cost < unvisited_node->cost) {
             unvisited_node->cost = cost;
           }
-          path.push_back(next_position);
+          neighbors.push_back({next_position, cost});
         }
       }
     }
-    return path;
+    return neighbors;
   };
 
   std::vector<node_t> unvisited;
@@ -82,36 +75,39 @@ void bfs(const std::vector<std::vector<int>>& grid, const offset_t& goal)
   }
   unvisited[0].cost = 0;
 
-  std::vector<offset_t> visited;
-  std::deque<offset_t> path;
-  path.push_back({0, 0});
+  std::vector<node_t> visited;
+  std::deque<node_t> path;
+  path.push_back({{0, 0}, 0});
 
   while (!path.empty()) {
     auto path_node = path.front();
-    if (path_node.r == goal.r && path_node.c == goal.c) {
-      break;
-    }
-    path.pop_front();
     visited.push_back(path_node);
-    auto neighbors = find_neighbors(path_node, visited, unvisited);
-    for (const auto& next_path_node : neighbors) {
+    path.pop_front();
+    auto neighbors = find_neighbors(path_node.offset, visited, unvisited);
+    for (const auto& neighbor : neighbors) {
       if (
         std::find_if(
           path.begin(), path.end(),
-          [next_path_node](const offset_t& node) {
-            return node.c == next_path_node.c && node.r == next_path_node.r;
+          [neighbor](const node_t& node) {
+            return node.offset.c == neighbor.offset.c
+                && node.offset.r == neighbor.offset.r;
           })
         == path.end()) {
-        path.push_back(next_path_node);
+        path.push_back(neighbor);
       }
     }
+    std::sort(path.begin(), path.end(), [](const auto& lhs, const auto& rhs) {
+      return lhs.cost < rhs.cost;
+    });
   }
 
-  std::cout
-    << "part 1: "
-    << unvisited[(grid.size() * (grid.size() - 1)) + (grid.back().size() - 1)]
-         .cost - 1
-    << '\n';
+  auto destination = std::find_if(
+    unvisited.begin(), unvisited.end(), [&grid](const node_t& unvisited_node) {
+      return unvisited_node.offset.c == grid.back().size() - 1
+          && unvisited_node.offset.r == grid.size() - 1;
+    });
+
+  return destination->cost;
 }
 
 int main([[maybe_unused]] int argc, [[maybe_unused]] char** argv)
@@ -131,14 +127,71 @@ int main([[maybe_unused]] int argc, [[maybe_unused]] char** argv)
   }
 
   // check
-  for (int row = 0; row < grid.size(); ++row) {
-    for (int col = 0; col < grid.back().size(); ++col) {
-      std::cout << grid[row][col] << ' ';
-    }
-    std::cout << '\n';
+  // for (int row = 0; row < grid.size(); ++row) {
+  //   for (int col = 0; col < grid.back().size(); ++col) {
+  //     std::cout << grid[row][col] << ' ';
+  //   }
+  //   std::cout << '\n';
+  // }
+
+  std::cout << "part 1: "
+            << bfs(grid, {int(grid.size() - 1), int(grid.back().size() - 1)})
+            << '\n';
+
+  std::vector<std::vector<int>> big_grid;
+  big_grid.resize(grid.size() * 5);
+  for (auto& row : big_grid) {
+    row.resize(grid.back().size() * 5);
   }
 
-  const offset_t goal = {int(grid.size() - 1), int(grid.back().size() - 1)};
+  for (int row = 0; row < grid.size(); ++row) {
+    for (int col = 0; col < grid.back().size(); ++col) {
+      big_grid[row][col] = grid[row][col];
+    }
+  }
 
-  bfs(grid, goal);
+  for (int outer_c = 1; outer_c < 5; ++outer_c) {
+    for (int row = 0; row < grid.size(); ++row) {
+      for (int col = 0; col < grid.back().size(); ++col) {
+        int r = row;
+        int c = grid.back().size() * outer_c + col;
+        int rb = row;
+        int cb = grid.back().size() * (outer_c - 1) + col;
+        int rv = big_grid[rb][cb];
+        rv++;
+        if (rv > 9) {
+          rv = 1;
+        }
+        big_grid[r][c] = rv;
+      }
+    }
+  }
+
+  for (int col = 0; col < big_grid.size(); ++col) {
+    for (int outer_row = 1; outer_row < 5; ++outer_row) {
+      for (int row = 0; row < grid.size(); ++row) {
+        int r = row + outer_row * grid.size();
+        int rb = row + (outer_row - 1) * grid.size();
+        int rv = big_grid[rb][col];
+        rv++;
+        if (rv > 9) {
+          rv = 1;
+        }
+        big_grid[r][col] = rv;
+      }
+    }
+  }
+
+  // for (int row = 0; row < big_grid.size(); ++row) {
+  //   for (int col = 0; col < big_grid.back().size(); ++col) {
+  //     std::cout << big_grid[row][col];
+  //   }
+  //   std::cout << '\n';
+  // }
+
+  std::cout << "part 2: "
+            << bfs(
+                 big_grid,
+                 {int(big_grid.size() - 1), int(big_grid.back().size() - 1)})
+            << "\n";
 }
