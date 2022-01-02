2370d8fe835c06ed11fa07e5c61f69c464e015c2
Tom Hulton-Harrop <tom.hultonharrop@gmail.com>
2022-01-02 17:32:14 +0000

    day 23, part 1 more experiments

--- day23/main.cpp
+++ day23/main.cpp
@@ -43,7 +43,17 @@ struct amphipod_t
   char type;
   int home;
 
-  //  bool at_home() const { return pos.x == home; }
+  [[nodiscard]] bool in_other_room() const
+  {
+    return in_room() && !in_home_room();
+  }
+  [[nodiscard]] bool in_room() const { return pos.y == 2 || pos.y == 3; }
+  [[nodiscard]] bool in_home_room() const { return pos.x == home; }
+  [[nodiscard]] bool in_base_home_room() const
+  {
+    return in_home_room() && pos.y == 3;
+  }
+  [[nodiscard]] bool in_hallway() const { return pos.y == 1; }
 };
 
 struct space_t
@@ -59,6 +69,15 @@ struct situation_t
   std::vector<vec2> hallway;
   std::vector<vec2> rooms;
   std::vector<amphipod_t> amphipods;
+
+  [[nodiscard]] bool goal_reached() const
+  {
+    bool all_home = true;
+    for (const auto amphipod : amphipods) {
+      all_home = amphipod.in_home_room() && all_home;
+    }
+    return all_home;
+  }
 };
 
 struct node_t
@@ -90,7 +109,7 @@ int sgn(T val)
   return (T(0) < val) - (val < T(0));
 }
 
-bool check(
+bool check_blocked(
   vec2 amphipod, vec2 delta, vec2 offset,
   const std::vector<space_t>& all_spaces)
 {
@@ -111,9 +130,9 @@ bool check(
   return false;
 }
 
-std::vector<node_t> find_hallway_positions(
+std::vector<node_t> find_positions(
   amphipod_t amphipod, const std::vector<space_t>& all_spaces,
-  const std::vector<vec2>& hallway)
+  const std::vector<vec2>& positions)
 {
   vec2 offsets[] = {{-1, -1}, {0, -1},  {1, -1},  {-1, 0},  {1, 0},  {-1, 1},
                     {0, 1},   {1, 1},   {-2, -2}, {-1, -2}, {0, -2}, {1, -2},
@@ -138,20 +157,20 @@ std::vector<node_t> find_hallway_positions(
         int y = 0;
         int x = 0;
         for (; y < std::abs(delta.y); ++y) {
-          blocked_1 |= check(amphipod.pos, {x, y}, delta, all_spaces);
+          blocked_1 |= check_blocked(amphipod.pos, {x, y}, delta, all_spaces);
         }
         for (; x < std::abs(delta.x); ++x) {
-          blocked_1 |= check(amphipod.pos, {x, y}, delta, all_spaces);
+          blocked_1 |= check_blocked(amphipod.pos, {x, y}, delta, all_spaces);
         }
       }
       {
         int y = 0;
         int x = 0;
         for (; x < std::abs(delta.x); ++x) {
-          blocked_2 |= check(amphipod.pos, {x, y}, delta, all_spaces);
+          blocked_2 |= check_blocked(amphipod.pos, {x, y}, delta, all_spaces);
         }
         for (; y < std::abs(delta.y); ++y) {
-          blocked_2 |= check(amphipod.pos, {x, y}, delta, all_spaces);
+          blocked_2 |= check_blocked(amphipod.pos, {x, y}, delta, all_spaces);
         }
       }
       if (blocked_1 && blocked_2) {
@@ -167,19 +186,19 @@ std::vector<node_t> find_hallway_positions(
             temp_positions.push_back(next_it->pos);
             visited.push_back(next);
           }
-          auto next_it_hall = std::find_if(
-            hallway.begin(), hallway.end(),
+          auto next_it_pos = std::find_if(
+            positions.begin(), positions.end(),
             [next](const auto hallway_pos) { return next == hallway_pos; });
-          if (next_it_hall != hallway.end()) {
+          if (next_it_pos != positions.end()) {
             auto hallway_exists = std::find_if(
               hallway_costs.begin(), hallway_costs.end(),
-              [next_it_hall](const auto hallway_cost) {
-                return *next_it_hall == hallway_cost.position;
+              [next_it_pos](const auto hallway_cost) {
+                return *next_it_pos == hallway_cost.position;
               });
             if (hallway_exists == hallway_costs.end()) {
               hallway_costs.push_back(
-                {*next_it_hall,
-                 manhattan_distance(amphipod.pos, *next_it_hall) * move_cost});
+                {*next_it_pos,
+                 manhattan_distance(amphipod.pos, *next_it_pos) * move_cost});
             }
           }
         }
@@ -187,6 +206,56 @@ std::vector<node_t> find_hallway_positions(
     }
   }
   return hallway_costs;
+}
+
+std::vector<node_t> find_room_positions(
+  amphipod_t amphipod, const std::vector<space_t>& all_spaces,
+  const std::vector<vec2>& rooms)
+{
+  // note: could then filter by slot/type
+  return find_positions(amphipod, all_spaces, rooms);
+}
+
+std::vector<node_t> find_hallway_positions(
+  amphipod_t amphipod, const std::vector<space_t>& all_spaces,
+  const std::vector<vec2>& hallway)
+{
+  return find_positions(amphipod, all_spaces, hallway);
+}
+
+void update_situation(
+  situation_t& situation, amphipod_t& amphipod, const node_t& node)
+{
+  auto prev_position = std::find_if(
+    situation.spaces.begin(), situation.spaces.end(),
+    [amphipod](const auto& space) { return space.pos == amphipod.pos; });
+  if (prev_position != situation.spaces.end()) {
+    prev_position->occupant = '.';
+  }
+  amphipod.pos = node.position;
+  auto next_position = std::find_if(
+    situation.spaces.begin(), situation.spaces.end(),
+    [node](const auto& space) { return space.pos == node.position; });
+  if (next_position != situation.spaces.end()) {
+    next_position->occupant = amphipod.type;
+  }
+}
+
+void process(situation_t situation)
+{
+  auto amphipod_it = std::find_if(
+    situation.amphipods.begin(), situation.amphipods.end(),
+    [](const amphipod_t amphipod) {
+      return !amphipod.in_base_home_room()
+          || (amphipod.in_room() && !amphipod.in_home_room())
+          || amphipod.in_hallway();
+    });
+
+  if (amphipod_it != situation.amphipods.end()) {
+    //    if (amphipod_it->
+  } else {
+    // in position
+  }
 }
 
 int main([[maybe_unused]] int argc, [[maybe_unused]] char** argv)
@@ -201,7 +270,7 @@ int main([[maybe_unused]] int argc, [[maybe_unused]] char** argv)
     std::cout << str << '\n';
   }
 
-  // need a 'state' that's the goal to check against
+  // need a 'state' that's the goal to check_blocked against
 
   situation_t situation;
   for (int row = 0; row < lines.size(); ++row) {
@@ -210,7 +279,8 @@ int main([[maybe_unused]] int argc, [[maybe_unused]] char** argv)
       if (cell == '.' && col != 3 && col != 5 && col != 7 & col != 9) {
         situation.spaces.push_back(space_t{vec2{col, row}, cell});
         situation.hallway.push_back(vec2{col, row});
-      } else if (cell == '.' && (col == 3 || col == 5 || col == 7 || col == 9)) {
+      } else if (
+        cell == '.' && (col == 3 || col == 5 || col == 7 || col == 9)) {
         situation.spaces.push_back(space_t{vec2{col, row}, cell});
       } else if (cell == 'A' || cell == 'B' || cell == 'C' || cell == 'D') {
         situation.spaces.push_back(space_t{vec2{col, row}, cell});
@@ -231,38 +301,54 @@ int main([[maybe_unused]] int argc, [[maybe_unused]] char** argv)
     }
   }
 
-  for (auto& amphipod : situation.amphipods) {
-    if (
-      (amphipod.type == 'A' && amphipod.pos == vec2{3, 3})
-      || (amphipod.type == 'B' && amphipod.pos == vec2{5, 3})
-      || (amphipod.type == 'B' && amphipod.pos == vec2{7, 3})
-      || (amphipod.type == 'B' && amphipod.pos == vec2{9, 3})) {
-      continue;
+  std::deque<node_t> path;
+
+  //  path.push_back()
+  std::vector<amphipod_t> starting_amphipods;
+  starting_amphipods.push_back(situation.amphipods[0]);
+  starting_amphipods.push_back(situation.amphipods[1]);
+  starting_amphipods.push_back(situation.amphipods[2]);
+  starting_amphipods.push_back(situation.amphipods[3]);
+
+  while (!starting_amphipods.empty()) {
+    auto amphipod = starting_amphipods.back();
+    starting_amphipods.pop_back();
+    if (amphipod.in_room() && !amphipod.in_base_home_room()) {
+      auto hallway_positions =
+        find_hallway_positions(amphipod, situation.spaces, situation.hallway);
+
+      for (const auto hallway : hallway_positions) {
+        path.push_back(hallway);
+      }
+
+      int i;
+      i = 0;
+    } else if (amphipod.in_hallway()) {
+      auto room_positions =
+        find_room_positions(amphipod, situation.spaces, situation.rooms);
+      int j;
+      j = 0;
     }
+  }
+
+  for (auto& amphipod : situation.amphipods) {
+    //    if (
+    //      (amphipod.type == 'A' && amphipod.pos == vec2{3, 3})
+    //      || (amphipod.type == 'B' && amphipod.pos == vec2{5, 3})
+    //      || (amphipod.type == 'B' && amphipod.pos == vec2{7, 3})
+    //      || (amphipod.type == 'B' && amphipod.pos == vec2{9, 3})) {
+    //      continue;
+    //    }
 
     auto hallway_positions =
       find_hallway_positions(amphipod, situation.spaces, situation.hallway);
 
-    auto hallway_positions_test = find_hallway_positions(
-      situation.amphipods[4], situation.spaces, situation.hallway);
+    auto room_positions_test = find_room_positions(
+      situation.amphipods[4], situation.spaces, situation.rooms);
 
     for (const auto possible_position : hallway_positions) {
       // calculate cost to next position
-      auto prev_position = std::find_if(
-        situation.spaces.begin(), situation.spaces.end(),
-        [amphipod](const auto& space) { return space.pos == amphipod.pos; });
-      if (prev_position != situation.spaces.end()) {
-        prev_position->occupant = '.';
-      }
-      amphipod.pos = possible_position.position;
-      auto next_position = std::find_if(
-        situation.spaces.begin(), situation.spaces.end(),
-        [possible_position](const auto& space) {
-          return space.pos == possible_position.position;
-        });
-      if (next_position != situation.spaces.end()) {
-        next_position->occupant = amphipod.type;
-      }
+      update_situation(situation, amphipod, possible_position);
       break;
     }
   }
